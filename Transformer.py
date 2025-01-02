7f9229c3797e9759af0bb8bdf8584fa33afedd2b
IsfarNayen <moriyaty133@gmail.com>
2025-01-03 00:42:47 +0600

    Reasons added for using pytorch and how to install pytorch in VSCode

--- Transformer.py
+++ Transformer.py
@@ -1,2 +1,5 @@
+#Needed to install pyTorch
+#Commands if there is a pytorch: pip list | findstr torch
+#For installing pytorch: pip install torch torchvision torchaudio
 import torch
 import torch.nn
